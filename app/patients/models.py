21d2cab0b4a282a02562671c002398a9fd38d53c
chakilamnaresh <chakilam.naresh@gmail.com>
2026-01-23 06:52:19 +0530

    added schemas

--- app/patients/models.py
+++ app/patients/models.py
@@ -2,14 +2,22 @@ from sqlalchemy import String,Date,Boolean,DateTime
 from sqlalchemy.orm import Mapped,mapped_column
 from datetime import datetime,date
 from app.db.base import Base
+from sqlalchemy.orm import relationship
 
 class Patient(Base):
     __tablename__ = "patients_data"
 
 
-    ids:Mapped[int] = mapped_column(primary_key=True)
+    id:Mapped[int] = mapped_column(primary_key=True)
     first_name:Mapped[str] = mapped_column(String(100))
     last_name:Mapped[str] = mapped_column(String(100))
     dob:Mapped[date] = mapped_column(Date)
     gender: Mapped[str] = mapped_column(String(10))
+    phone:Mapped[str] = mapped_column(String(10))
+
+
+    user = relationship("User",backref="patient_profile")
+
+
+
 
